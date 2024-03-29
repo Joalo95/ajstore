e0011293ceabf3841cc0a0c00b13e59982ff7c95
joaquin <joalonsort@gmail.com>
2024-03-28 23:08:28 -0300

    agrego proyecto

---
+++ src/app/payment/page.tsx
@@ -0,0 +1,37 @@
+import MercadPagoIcon from "@/components/icons/mercadoPago";
+import { Button } from "@/components/ui/button";
+import { useCart } from "@/modules/cart/context/client";
+import { CartItem } from "@/modules/cart/types";
+
+export function CartButtonPay() {
+    const [{ cart }] = useCart();
+
+    const items: CartItem[] = Array.from(cart.values());
+
+    async function payOrder() {
+        try {
+            const res = await fetch('/payment', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ items }),
+            });
+            const { url } = await res.json();
+            window.open(url, '_blank');
+        } catch (e) {
+            console.log("Error en el servidor", e);
+        }
+    }
+
+    return (
+        <div className="flex">
+            <Button className="w-full" size="lg" onClick={payOrder}>
+                <div className="inline-flex items-center gap-2">
+                    <MercadPagoIcon />
+                    <span>Pagar</span>
+                </div>
+            </Button>
+        </div>
+    );
+}
