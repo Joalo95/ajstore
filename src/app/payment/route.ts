18b5660e909e3ffee0756ef4978bb2c97771986f
joaquin <joalonsort@gmail.com>
2024-03-31 21:19:00 -0300

    agrego hero

--- src/app/payment/route.ts
+++ src/app/payment/route.ts
@@ -1,16 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
-import { MercadoPagoConfig, Preference, Payment } from "mercadopago";
+
+import { MercadoPagoConfig, Preference } from "mercadopago";
 
 const client = new MercadoPagoConfig({
     accessToken: process.env.MP_ACCESS_TOKEN!,
     options: { timeout: 5000 }
 });
 
+interface Item {
+    id: string;
+    title: string;
+    description: string;
+    quantity: number;
+    price: number;
+}
+
+interface ProcessedItem {
+    id: string;
+    title: string;
+    description: string;
+    quantity: number;
+    unit_price: number;
+}
+
 export async function POST(req: NextRequest) {
 
-    const items = await req.json();
+    const items: { items: Item[] } = await req.json();
     const itemsArray = items.items;
-    const processedItems = itemsArray.map(({ id, title, description, quantity, price }) => ({ id, title, description, quantity, unit_price: price }));
+    const processedItems: ProcessedItem[] = itemsArray.map(({ id, title, description, quantity, price }) => ({ id, title, description, quantity, unit_price: price }));
 
     try {
         const preference = await new Preference(client).create({
@@ -18,10 +35,9 @@ export async function POST(req: NextRequest) {
                 items: processedItems,
             },
         });
-        //console.log(items);
+
         return NextResponse.json({ url: preference.sandbox_init_point });
     } catch (e) {
-        //console.log("Error en el servidor", e);
         return NextResponse.error({ status: 500, statusText: e.message });
     }
 }
